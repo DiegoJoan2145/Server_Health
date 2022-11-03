c345d1116aca9338f202d995753c82d2ea527375
DiegoJoan2145 <diegosalazarh90@gmail.com>
2022-11-02 19:03:43 -0600

    Modificacion

--- Server/controllers/commentsController.js
+++ Server/controllers/commentsController.js
@@ -20,32 +20,36 @@ exports.createComments = async (req, res) => {
             return res.status(400).json({ message: 'Sorry, all fields are required' });
         }
 
-        const Comentarios = await comentario.create({
-            titulo: titulo,
-            descripcion: descripcion,
-            respuesta: respuesta,
-            idUsuario: idUsuario,
-        });
-
-        if(!Comentarios.respuesta){
-            let nuevaResp = 'No hay respuestas';
-            const Respuesta = {
-                _id: Comentarios._id,
-                titulo: Comentarios.titulo,
-                descripcion: Comentarios.descripcion,
-                respuesta: nuevaResp,
-            }
-            return res.send(Respuesta);
-        } else {
-            const Respuesta = {
-                _id: Comentarios._id,
-                titulo: Comentarios.titulo,
-                descripcion: Comentarios.descripcion,
+        async function Guardar(titulo, descripcion, respuesta, idUsuario){
+            const Comentarios = await comentario.create({
+                titulo: titulo,
+                descripcion: descripcion,
                 respuesta: respuesta,
-            }
-            return res.send(Respuesta)
+                idUsuario: idUsuario,
+            });
+            return Comentarios;
         }
 
+        if(!respuesta){
+            let respuesta = 'No hay respuestas';
+            Guardar(
+                titulo,
+                descripcion,
+                respuesta,
+                idUsuario
+            ).then(resp =>{
+                return res.status(200).json(resp);
+            })
+        } else {
+            Guardar(
+                titulo,
+                descripcion,
+                respuesta,
+                idUsuario
+            ).then(resp =>{
+                return res.status(200).json(resp);
+            })
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).send('There is an error with server');
@@ -66,26 +70,10 @@ exports.showComments = async (req, res) => {
 
         const user = await comentario.find({
                     idUsuario: idUsuario
-        })
+        });
 
-        if(!user[0].respuesta){
-            let nuevaResp = 'No hay respuestas';
-            const Respuesta = {
-                _id: user[0]._id,
-                titulo: user[0].titulo,
-                descripcion: user[0].descripcion,
-                respuesta: nuevaResp,
-            }
-            return res.send(Respuesta);
-        } else {
-            const Respuesta = {
-                _id: user[0]._id,
-                titulo: user[0].titulo,
-                descripcion: user[0].descripcion,
-                respuesta: user[0].respuesta,
-            }
-            return res.send(Respuesta)
-        }
+        return res.status(200).json(user);
+        
 
     } catch (error) {
         console.log(error);
